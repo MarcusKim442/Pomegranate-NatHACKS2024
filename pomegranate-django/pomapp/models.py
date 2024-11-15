acb7c9c31251a2699c0b5a61f282f8890d5d1206
chidinmaamogu <amogu@ualberta.ca>
2024-11-15 16:07:11 -0700

    potential for posting multiple photos rn, not working properly

--- pomegranate-django/pomapp/models.py
+++ pomegranate-django/pomapp/models.py
@@ -1,3 +1,16 @@
+from datetime import timezone
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    profile_photo = models.CharField(max_length=1000, blank=False)
+    user = models.CharField(max_length = 20)
+    title = models.CharField(max_length=100, blank=False)
+    caption = models.CharField(max_length=500, blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+class PostImage(models.Model):
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    image = models.CharField(max_length=1000, blank=False)
--- pomegranate-django/pomapp/views.py
+++ pomegranate-django/pomapp/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from pomapp.models import Post, PostImage
+import json
 
 def home(request):
     return render(request, 'home.html')
+
+def view_post(request, id):
+    post = Post.objects.get(id=id)
+    next_id = post.id + 1
+    prev_id = post.id - 1
+    next_post = Post.objects.get(id=next_id)
+    previous_post = Post.objects.get(id=prev_id)
+
+    imageObjects = PostImage.objects.filter(post_id = post)
+    images = []
+    for imageO in imageObjects:
+        images.append(imageO.image)
+
+    return render(request, 'post.html', {
+        "post": post,
+        "image_list": images,
+        "next_post": next_post,
+        "previous_post": previous_post
+    })
