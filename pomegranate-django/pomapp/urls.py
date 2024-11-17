e6db82ab7a3b9997c305f4600539498593be372d
nabiii000 <gshin1@ualberta.ca>
2024-11-17 09:03:40 -0700

    updated

--- pomegranate-django/pomapp/urls.py
+++ pomegranate-django/pomapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('pomapp', views.home, name='home'),
     path('pomapp/post/<int:id>', views.view_post, name='view_post'),
     path('poll', views.poll_updates, name='poll'),
     path('begin-collection', views.send_eeg_collection_trigger, name='begin-collection'),
